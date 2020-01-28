c16b47f80e163909a5dd6f31150ee1ca00418caf
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2020-01-28 10:36:45 +0000

    28/01/2019@10:36

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('button');
+
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
+}
+
+btn.onclick = function() {
+  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}
+
+//How about using this to make a dark/light mode for your portfolio site? Other ideas you could explore with this?
